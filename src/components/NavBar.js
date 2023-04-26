51b3b162330bff56d666d8cb9c1f75fc90f775f7
Luiz Costa/BRA <Luiz.E.Costa@cushwake.com>
2023-04-25 22:32:50 -0300

    first version

---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router-dom"
+import styles from './NavBar.module.css'
+
+const NavBar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <NavLink className={styles.brand} to="/">
+                Mini <span>Blog</span>
+            </NavLink>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) => (isActive ? styles.active : "")}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/login"
+                        className={({ isActive }) => (isActive ? styles.active : "")}
+                    >
+                        Entrar
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/register"
+                        className={({ isActive }) => (isActive ? styles.active : "")}
+                    >
+                        Cadastrar
+                    </NavLink>
+                </li>
+                
+                
+                <li>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) => (isActive ? styles.active : "")}
+                    >
+                        Sobre
+                    </NavLink>
+                </li>
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
